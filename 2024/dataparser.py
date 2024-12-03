574bae31a25d5ad0883117ec901e0766b06cf8ef
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2024-12-03 20:38:58 +0100

    Delete dataparser.py

--- 2024/dataparser.py
+++
@@ -1,7 +0,0 @@
-def get_data(file_name):
-    with open(file_name, 'r') as file:
-        data = file.read()
-
-    lines = data.splitlines()
-    rows = [tuple(map(int, line.split())) for line in lines]
-    return rows
