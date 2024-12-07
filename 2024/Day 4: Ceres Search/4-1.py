defdec0d2911f129d3f6a3bdc66929de07405cdc
XinyuJi <56640682+XinyuJi@users.noreply.github.com>
2024-12-07 09:08:40 +0100

    2024.12.07

--- 2024/Day 4: Ceres Search/4-1.py
+++ 2024/Day 4: Ceres Search/4-1.py
@@ -1,10 +1,8 @@
 with open('test.txt', 'r') as file:
     data = file.read()
-grid = data.splitlines()
+map = data.splitlines()
 
 word = "XMAS"
-word_length = len(word)
-
 directions = [
     (0, 1),    # Right
     (0, -1),   # Left
@@ -16,29 +14,22 @@ directions = [
     (-1, 1)    # Up-Right
 ]
 
-def search_direction(grid, word, row, col, direction):
-    row_step, col_step = direction
-    for i in range(word_length):
-        r, c = row + i * row_step, col + i * col_step
-        if not (0 <= r < len(grid) and 0 <= c < len(grid[0])):
-            return False
-        if grid[r][c] != word[i]:
-            return False
-    return True
 
-def find_word(grid, word):
-    results = []
-    for row in range(len(grid)):
-        for col in range(len(grid[0])):
-            for direction in directions:
-                if search_direction(grid, word, row, col, direction):
-                    results.append(((row, col), direction))
-    return results
-
-
-matches = find_word(grid, word)
-print(len(matches))
-
-# for match in matches:
-#     start, direction = match
-#     print(f"Found 'XMAS' starting at {start} in direction {direction}")
+result = []
+for row in range(len(map)):
+    for col in range(len(map[0])):
+        for dir in directions:
+            row_step, col_step = dir
+            check = 0
+            for i in range(len(word)):
+                r, c = row + i*row_step, col + i *col_step
+                if not (0 <= r < len(map) and 0 <= c < len(map[0])):
+                    check = 1
+                    break
+                if map[r][c] != word[i]:
+                    check = 1
+                    break
+            if check == 0:
+                result.append(((row, col), dir))
+print(result)
+print(len(result))
---
+++ Day 7: Bridge Repair/7-1.py
@@ -0,0 +1,32 @@
+from itertools import product
+
+def evaluate_expression(nums, operators):
+    result = nums[0]
+    for i, op in enumerate(operators):
+        if op == '+':
+            result += nums[i + 1]
+        elif op == '*':
+            result *= nums[i + 1]
+    return result
+
+def can_form_target(target, nums):
+    operators = list(product(['+', '*'], repeat=len(nums) - 1))
+    for ops in operators:
+        if evaluate_expression(nums, ops) == target:
+            return True
+    return False
+
+with open('test.txt', 'r') as file:
+    data = file.read()
+data_dict = {}
+lines = data.strip().split("\n")
+for line in lines:
+    key, values = line.split(":")
+    data_dict[int(key)] = list(map(int, values.split()))
+
+total = 0
+for target, nums in data_dict.items():
+    if can_form_target(target, nums):
+        total += target
+    # print(f"{target}: {result}")
+print(total)
